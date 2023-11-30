45d2f8b2ebd1a0db219bb4fbfe4000263d8ea529
Leila <leilazrim@gmail.com>
2023-11-30 23:39:45 +0100

    Implement rudimentary clickhandler and associate the cell

--- index.js
+++ index.js
@@ -21,3 +21,56 @@ function drawCells(x, y) {
     console.log("🚀 ~ file: index.js:18 ~ drawCells ~ x:", x);
   }
 }
+
+field.addEventListener("click", (e) => {
+  let xCord = e.offsetX;
+  let yCord = e.offsetY;
+  console.log(`x: ${xCord}/ y: ${yCord}`);
+  console.log(calculateBounds(xCord, yCord));
+});
+
+//first x: 0 - cellsz y: 0 - cellsz
+function calculateBounds(x, y) {
+  let cellNum = -1;
+  // 0 - cell_sz
+  if (between(y, 0, cell_sz)) {
+    if (between(x, 0, cell_sz)) {
+      cellNum = 0;
+    }
+    if (between(x, cell_sz, cell_sz * 2)) {
+      cellNum = 1;
+    }
+    if (between(x, cell_sz * 2, cell_sz * 3)) {
+      cellNum = 2;
+    }
+  }
+
+  if (between(y, cell_sz, cell_sz * 2)) {
+    if (between(x, 0, cell_sz)) {
+      cellNum = 3;
+    }
+    if (between(x, cell_sz, cell_sz * 2)) {
+      cellNum = 4;
+    }
+    if (between(x, cell_sz * 2, cell_sz * 3)) {
+      cellNum = 5;
+    }
+  }
+  if (between(y, cell_sz * 2, cell_sz * 3)) {
+    if (between(x, 0, cell_sz)) {
+      cellNum = 6;
+    }
+    if (between(x, cell_sz, cell_sz * 2)) {
+      cellNum = 7;
+    }
+    if (between(x, cell_sz * 2, cell_sz * 3)) {
+      cellNum = 8;
+    }
+  }
+
+  return cellNum;
+}
+
+function between(num, min, max) {
+  return num > min && num < max;
+}
