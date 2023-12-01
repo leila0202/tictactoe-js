fc362f704f66b80ad6872b1d18571de8d6b031dd
Leila <leilazrim@gmail.com>
2023-12-01 01:02:36 +0100

    implement first placing of the cross

--- index.js
+++ index.js
@@ -1,71 +1,106 @@
 const field = document.getElementById("field");
 const ctx = field.getContext("2d");
+let player = "X";
 let width = field.width;
 let height = field.height;
 
 const cell_sz = width / 3;
-console.log("🚀 ~ file: index.js:7 ~ cell_sz:", cell_sz);
 
-ctx.beginPath();
 drawCells(cell_sz, height);
 for (let i = 1; i <= 2; i++) {
+  ctx.beginPath();
   ctx.moveTo(0, cell_sz * i);
   ctx.lineTo(width, cell_sz * i);
+  ctx.stroke();
 }
-ctx.stroke();
 
 function drawCells(x, y) {
+  console.log("should only run once");
   for (let i = 1; i <= 2; i++) {
+    ctx.beginPath();
+    console.log(i);
     ctx.moveTo(x * i, 0);
     ctx.lineTo(x * i, y);
-    console.log("🚀 ~ file: index.js:18 ~ drawCells ~ x:", x);
+    ctx.stroke();
   }
 }
 
 field.addEventListener("click", (e) => {
   let xCord = e.offsetX;
+  console.log(
+    "🚀 ~ file: index.js:26 ~ field.addEventListener ~ xCord:",
+    xCord
+  );
   let yCord = e.offsetY;
-  console.log(`x: ${xCord}/ y: ${yCord}`);
+  console.log(
+    "🚀 ~ file: index.js:28 ~ field.addEventListener ~ yCord:",
+    yCord
+  );
+
   console.log(calculateBounds(xCord, yCord));
 });
+
+const OFFSET = 4;
+
+function drawCross(startX, startY) {
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = "10";
+  ctx.beginPath();
+  ctx.moveTo(startX + OFFSET, startY + OFFSET);
+  ctx.lineTo(startX + cell_sz - OFFSET, startY + cell_sz - OFFSET);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(startX + cell_sz - OFFSET, startY + OFFSET);
+  ctx.lineTo(startX + OFFSET, startY + cell_sz - OFFSET);
+  ctx.stroke();
+  ctx.strokeStyle = "black";
+}
 
 //first x: 0 - cellsz y: 0 - cellsz
 function calculateBounds(x, y) {
   let cellNum = -1;
+  let xCord = 0;
+  let yCord = 0;
   // 0 - cell_sz
   if (between(y, 0, cell_sz)) {
     if (between(x, 0, cell_sz)) {
-      cellNum = 0;
+      xCord = 0;
+      yCord = 0;
     }
     if (between(x, cell_sz, cell_sz * 2)) {
-      cellNum = 1;
+      xCord = cell_sz;
     }
     if (between(x, cell_sz * 2, cell_sz * 3)) {
-      cellNum = 2;
+      xCord = cell_sz * 2;
     }
   }
 
   if (between(y, cell_sz, cell_sz * 2)) {
+    yCord = cell_sz;
     if (between(x, 0, cell_sz)) {
-      cellNum = 3;
+      xCord = 0;
     }
     if (between(x, cell_sz, cell_sz * 2)) {
-      cellNum = 4;
+      xCord = cell_sz;
     }
     if (between(x, cell_sz * 2, cell_sz * 3)) {
-      cellNum = 5;
+      xCord = cell_sz * 2;
     }
   }
   if (between(y, cell_sz * 2, cell_sz * 3)) {
+    yCord = cell_sz * 2;
     if (between(x, 0, cell_sz)) {
-      cellNum = 6;
+      xCord = 0;
     }
     if (between(x, cell_sz, cell_sz * 2)) {
-      cellNum = 7;
+      xCord = cell_sz;
     }
     if (between(x, cell_sz * 2, cell_sz * 3)) {
-      cellNum = 8;
+      xCord = cell_sz * 2;
     }
+  }
+  if (player == "X") {
+    drawCross(xCord, yCord);
   }
 
   return cellNum;
